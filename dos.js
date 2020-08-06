b663b62d1495179e3adcf2ce892ae6b0bb80e11d
Emanuel <67979947+EmanuelCenturion@users.noreply.github.com>
2020-08-05 22:24:47 -0300

    Add files via upload

--- dos.js
+++ dos.js
@@ -12,5 +12,42 @@ Considerar que las categorías pueden ser: almacen, lácteos, limpieza o bebidas
 
 function mostrar()
 {
-	alert("Hola Mundo");
+	let nombreArticulo;
+	let categoria;
+	let precio;
+	let respuesta = 'n';
+	let cantArticulos;
+	let articulos;
+	
+	prompt("desea ingresar un articulo? s/n");
+
+	while(respuesta == 's'){
+		alert("las categorias validas son almacen, lácteos, limpieza o bebidas y el precio no puede ser menor a 0 ni mayor a 1000");   
+		
+		nombreArticulo = prompt("ingrese nombre de articulo:");
+		
+		if(precio>0 && precio<1000){
+		precio = parseInt(prompt("ingrese precio de articulo:"));
+		}else{
+			alert("precio fuera de rango");
+		}
+		
+		switch(categoria){
+			case almacen:
+			case lácteos:
+			case limpieza:
+			case bebidas:
+				categoria = prompt("ingrese categoria de articulo:");
+				break;
+			default:
+				alert("categoria no valida");
+		}
+		cantArticulos++;
+		articulos += nombreArticulo;
+
+		
+	}
+
+	alert("la cantidad de articulos ingresados es de "+cantArticulos);
+	
 }
